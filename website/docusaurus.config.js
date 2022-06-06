b4dec8b8a1f5fccae26360195f6789acadecb037
Reiji Tokuda <tokuda@rtez.net>
2022-06-06 21:56:34 +0800

    Fix config and github action

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -16,8 +16,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "rtez", // Usually your GitHub org/user name.
-  projectName: "githubpages", // Usually your repo name.
+  organizationName: "stoikheia", // Usually your GitHub org/user name.
+  projectName: "stoikheia.github.io", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
